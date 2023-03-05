73117e86e0848ea7e8798031df693947b726bed3
ZisongChen <98493713+ZisongChen@users.noreply.github.com>
2023-03-05 23:26:34 +0800

    Update Edit.js

--- client/src/pages/Edit.js
+++ client/src/pages/Edit.js
@@ -4,7 +4,7 @@ import { useLocation } from 'react-router-dom';
 const Edit = (props) => {
    const [postList, setPostList] = useState([]);
    const { search } = useLocation();
-  const queryParams = new URLSearchParams(search);
+  const queryParams = new URLSearchParams(search);//for getting the params send from above page
   const postId = queryParams.get('postId');
   var id={"postId":postId}
     const [neww, setneww] = useState({
@@ -15,8 +15,8 @@ const Edit = (props) => {
 
       const handleChange = e => {
         setneww({ ...neww, [e.target.id]: e.target.value });
-      };
-      var token=localStorage.getItem("auth_token");
+      };//function for change related thing in neww
+      var token=localStorage.getItem("auth_token");//authentication is stored in localstorage
       const handleclick = async (event) => {
         console.log(neww)
         const a = await fetch('http://localhost:1234/api/editpost', {
@@ -35,7 +35,7 @@ const Edit = (props) => {
         <>
         <h1>My Data</h1>
         <h1>Edit for Post {postId}</h1>
-          {localStorage.getItem("auth_token")?(
+          {localStorage.getItem("auth_token")?(//if you login then show following stuff, they are the component to edit posts
             <>
             <TextField label="new title" variant='outlined' color='secondary' id="title"  value={neww.title} onChange={handleChange}/>
             <TextField label="new word" variant='outlined' color='secondary' id="word"  value={neww.word} onChange={handleChange}/>
