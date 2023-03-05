72aaa8947726c669689fa25224de4db1d11484b1
ZisongChen <98493713+ZisongChen@users.noreply.github.com>
2023-03-06 04:53:48 +0800

    Update test2-register.cy.js

--- cypress/e2e/test2-register.cy.js
+++ cypress/e2e/test2-register.cy.js
@@ -2,7 +2,8 @@ describe('search', () => {
   it('passes', () => {
     cy.visit('http://localhost:3000/')
     cy.get('[href="/register"]').click()
-    
+    cy.get('#email').type("aazz")
+    cy.get('#password').type("zz")
     cy.get('#register').click()
   })
 })
